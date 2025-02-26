625cd41edc1430fb3449a6de27d0bd82b2a1d83f
Manish Chauhan <manish.chauhan@ushatech.in>
2025-02-26 06:00:07 +0530

    enhanced for loop usage

--- src/Main.kt
+++ src/Main.kt
@@ -10,5 +10,55 @@ fun main() {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
+
     }
+    printMyName("Kotlin")
+
+    useStringInterpolation()
+
+    createLoops()
+
+    useConditions()
+}
+
+fun useConditions() {
+    val x = 20
+    if(x == 20 )
+    {
+        println("x is less than 20")
+    }
+    else
+    {
+        println("x is not less than 20")
+    }
+    val name = "Kotlin"
+    if(name.equals("Kotlin"))
+    {
+        println("name is Kotlin")
+    }
+}
+
+fun createLoops() {
+    var x = 20
+    while (x <= 20) {
+        x=x+1
+        print("$x")
+    }
+
+    for(i in 1..10) {
+        print("$i")
+    }
+
+}
+
+fun useStringInterpolation() {
+    var x = 3
+    val name = "Kotlin"
+    x += 10
+    println("$name x is $x.")
+    // This is a comment
+}
+
+fun printMyName(s: String) {
+    println(s)
 }
